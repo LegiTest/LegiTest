f87bbfcf198fb9308db9b980cc830d0a1fdecac1
QuelParti <89925832+QuelParti@users.noreply.github.com>
2021-10-04 16:39:05 +0200

    do not return a 500 in case of CritAbuseIpDbApiFail

--- src/errors.rs
+++ src/errors.rs
@@ -83,6 +83,9 @@ pub enum ErrorKind {
 impl ErrorKind {
     // returns true for items that must display only if debug mode is enabled
     // will return 200 OK for those items if config hide_errors == true
+    
+    // NOTE: CritAbuseIpDbApiFail is special, because the error is
+    // non-blocking and handled server-side.
     pub fn is_info(&self) -> bool {
         matches!(
             self,
@@ -92,6 +95,7 @@ impl ErrorKind {
                 | ErrorKind::InfoAlreadyPolled
                 | ErrorKind::InfoIpBlocklisted
                 | ErrorKind::InfoAsnBlocklisted
+                | ErrorKind::CritAbuseIpDbApiFail
         )
     }
 
@@ -111,6 +115,7 @@ impl ErrorKind {
                 | ErrorKind::WarnResultsNoPlatform
                 | ErrorKind::WarnCsrfTokenInvalid
                 | ErrorKind::WarnInvalidAnswer
+                | ErrorKind::CritAbuseIpDbApiFail
         )
     }
 }
