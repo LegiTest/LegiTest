6c67b2abd21356e5bb26b28a10bacaa17653836e
QuelParti <89925832+QuelParti@users.noreply.github.com>
2021-11-05 22:19:58 +0100

    not counting display=false groups. fixes #13

--- src/reports.rs
+++ src/reports.rs
@@ -1,6 +1,7 @@
 use chrono::NaiveDate;
 use std::collections::HashMap;
 
+use crate::config::structs::InstanceInfo;
 use crate::config::structs::Platform;
 use crate::database::structs::{Choice, ResultsGroupes, Submissions};
 use crate::database::views::{
@@ -26,25 +27,25 @@ pub fn generate_report(
     // 1. check if the poll is still open
     if platform.end_at < generated_at {
         return Err(throw(
-            ErrorKind::InfoPollClosed,
-            format!("{}: {} - {}", platform.id, platform.end_at, generated_at),
+                ErrorKind::InfoPollClosed,
+                format!("{}: {} - {}", platform.id, platform.end_at, generated_at),
         ));
     }
 
     // 2. get a count of the valid submissions
     let valid_sub_count =
         Submissions::count_valid(platform.id, conn, platform.begin_at, generated_at)
-            .map_err(|e| throw(ErrorKind::CritReportCountValid, e.to_string()))?;
+        .map_err(|e| throw(ErrorKind::CritReportCountValid, e.to_string()))?;
 
     // 3. check if the poll has reached its minimum participations
     if valid_sub_count < i64::from(platform.minimum_participations) {
         // if it didn't, do not generate a report
         return Err(throw(
-            ErrorKind::InfoNotEnoughSubs,
-            format!(
-                "{}: {} / {}",
-                platform.id, valid_sub_count, platform.minimum_participations
-            ),
+                ErrorKind::InfoNotEnoughSubs,
+                format!(
+                    "{}: {} / {}",
+                    platform.id, valid_sub_count, platform.minimum_participations
+                ),
         ));
     }
 
@@ -55,7 +56,7 @@ pub fn generate_report(
     // 5. get all the valid submissionschoices!
     let subchoices_list =
         Submissions::get_valid(platform.id, conn, platform.begin_at, generated_at)
-            .map_err(|e| throw(ErrorKind::CritReportGetValid, e.to_string()))?;
+        .map_err(|e| throw(ErrorKind::CritReportGetValid, e.to_string()))?;
 
     // 6. basically, do a GROUP BY
     let mut submissions_list: HashMap<i64, Vec<UserOpinion>> = HashMap::new();
@@ -67,8 +68,8 @@ pub fn generate_report(
             None => {
                 // should never happen
                 return Err(throw(
-                    ErrorKind::FatalUnmatchedChoice,
-                    subc.userchoice.to_string(),
+                        ErrorKind::FatalUnmatchedChoice,
+                        subc.userchoice.to_string(),
                 ));
             }
         };
@@ -82,8 +83,8 @@ pub fn generate_report(
                 }),
                 None => {
                     return Err(throw(
-                        ErrorKind::FatalMissingSubmission,
-                        format!("{:?}", subc),
+                            ErrorKind::FatalMissingSubmission,
+                            format!("{:?}", subc),
                     ));
                 }
             }
@@ -104,8 +105,8 @@ pub fn generate_report(
             Some(v) => v,
             None => {
                 return Err(throw(
-                    ErrorKind::FatalInvalidScoreCalc,
-                    format!("{:?}", scores),
+                        ErrorKind::FatalInvalidScoreCalc,
+                        format!("{:?}", scores),
                 ));
             }
         };
@@ -115,8 +116,8 @@ pub fn generate_report(
             Some(v) => v,
             None => {
                 return Err(throw(
-                    ErrorKind::FatalInvalidScoreConv,
-                    format!("{:?}", scores),
+                        ErrorKind::FatalInvalidScoreConv,
+                        format!("{:?}", scores),
                 ));
             }
         };
@@ -127,7 +128,6 @@ pub fn generate_report(
     let mut results_groups: Vec<ResultsPublicGroupes> = Vec::new();
 
     for group in &platform.groups {
-
         let group_scores = get_group_scores(*group, &all_scores);
         results_groups.push(ResultsPublicGroupes {
             id: *group,
@@ -180,7 +180,6 @@ fn get_group_scores(group_id: i16, all_scores: &HashMap<i64, Vec<GroupMatch>>) -
 
 // Score calculation method: Median
 fn calc_median(group_allscores: &[f32]) -> f32 {
-
     // return the median
     if let Some(v) = group_allscores.get(group_allscores.len() / 2) {
         *v
@@ -192,7 +191,6 @@ fn calc_median(group_allscores: &[f32]) -> f32 {
         0.0
     }
 }
-
 
 // Score calculation method: Mean
 fn calc_avg(group_allscores: &[f32]) -> f32 {
@@ -211,40 +209,60 @@ fn calc_avg(group_allscores: &[f32]) -> f32 {
 // if best match -> +1, if not -> 0.
 // if best match along with OTHER best matches -> +(1 / best_matches_list.len())
 fn calc_uninominal(group_id: i16, all_scores: &HashMap<i64, Vec<GroupMatch>>) -> f32 {
-    
+    let g_instance = InstanceInfo::global();
+
     let mut group_score = 0_f32;
 
     for submission in all_scores.values() {
-        let mut max_sub: Vec<GroupMatch> = submission.to_vec();
+        let max_sub: Vec<GroupMatch> = submission.to_vec();
+
+        // filter out groups with display=false
+        let mut max_sub = max_sub.iter()
+            .filter(|a| {
+                let ni = g_instance.acteurs_list.organes.iter().find(|g| g.id == a.id)
+                    .expect("");
+                ni.display
+            }).collect::<Vec<&GroupMatch>>();
 
         // sort the group affinities
         max_sub.sort_by(|a, b| {
-            b.affinity.partial_cmp(&a.affinity)
+            b.affinity
+                .partial_cmp(&a.affinity)
                 .expect("calc_uninominal: There's a NaN!!!")
         });
 
         // take only the first ones with an identical score
-        let best_matches_list: Vec<&GroupMatch> = max_sub.iter().filter(|a| {
-            a.affinity >= max_sub.first().expect("calc_uninominal: no first entry in scores array").affinity
-        }).collect();
+        let best_matches_list: Vec<&&GroupMatch> = max_sub
+            .iter()
+            .filter(|a| {
+                a.affinity
+                    >= max_sub
+                    .first()
+                    .expect("calc_uninominal: no first entry in scores array")
+                    .affinity
+            })
+        .collect();
 
         // if the selected group is among the best matches,
         // increment its score: divide 1.0 with the number of ex-aequo groups.
         // ex.: alone = 1, two winners = 0.5, three = 0.33...
         // else, the selected group isn't among the best matches: skip.
-        if best_matches_list.iter().find(|&m| m.id == group_id).is_some() {
+        if best_matches_list
+            .iter()
+                .find(|&m| m.id == group_id)
+                .is_some()
+        {
             match best_matches_list.len() {
                 // directly add 1.0 if the group is alone.
                 1 => group_score += 1.0,
                 // also handle a case that should never happen.
-                0 => { 
+                0 => {
                     eprintln!("calc_uninominal attempted to divide by zero??!");
-                    continue
-                },
+                    continue;
+                }
                 _ => group_score += 1.0 / (best_matches_list.len() as f32),
             };
         }
-
     }
     // return a %
     group_score / (all_scores.len() as f32) * 100.0
